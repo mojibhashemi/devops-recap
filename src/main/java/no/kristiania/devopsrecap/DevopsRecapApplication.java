7a81037536ba10d1f888a41eed1fd56cf0b6e846
mojibhashemi <mojib_hashemi@hotmail.com>
2022-10-11 16:43:00 +0200

    init project

---
+++ src/main/java/no/kristiania/devopsrecap/DevopsRecapApplication.java
@@ -0,0 +1,13 @@
+package no.kristiania.devopsrecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsRecapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/kristiania/devopsrecap/DevopsRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package no.kristiania.devopsrecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsRecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
